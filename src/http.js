a41618bea00b86da16350d9d02aceb7c161b28d6
Quildreen Motta <quildreen@gmail.com>
2012-04-19 22:14:26 -0300

    Adds documentation for the HTTP module.

--- src/http.js
+++ src/http.js
@@ -40,6 +40,12 @@ var Promise            = cassie.Promise
 
 
 //// -- Helpers ---------------------------------------------------------------
+
+///// Function make_xhr
+// Creates a new XMLHttpRequest object that can be used for the current
+// engine.
+//
+// make-xhr :: () -> XMLHttpRequest
 var make_xhr = function() {
                  return 'XMLHttpRequest' in this?
                         /* W3C? */ function() {
@@ -48,39 +54,92 @@ var make_xhr = function() {
                         /* IE? */  function() {
                                      return new ActiveXObject('Microsoft.XMLHTTP') }}()
 
+
+///// Function object_p
+// Is the given `subject' an `Object'?
+//
+// object? :: a -> Bool
+function object_p(subject) {
+  return class_of(subject) == '[object Object]' }
+
+
+///// Function status_type
+// Returns the firendly HTTP status name for the class of HTTP statuses
+// the given `status' belongs to.
+//
+// status-type :: Number -> String
+function status_type(status) {
+  var type = status.toString().charAt(0) - 1
+  return statuses[type] }
+
+
+///// Function serialise_for_type
+// Serialises the given data according to the specified MIME type.
+//
+// serialise-for-type :: String, { String -> String } -> String
+function serialise_for_type(mime, data) {
+  return mime == 'application/json'?  JSON.stringify(data)
+  :      /* otherwise */              serialise(data) }
+
+
+
+// Whether the engine supports XHR2's `timeout' attribute
+//
+// support-timeout? :: Bool
 var support_timeout_p = 'timeout' in make_xhr()
 
+// A regular expression matching successful HTTP response codes
+//
+// success :: RegExp
 var success = /2\d{2}/
-var error   = /[45]\d{2}/
 
+// A regular expression matching client and server error HTTP response
+// codes.
+//
+// error :: RegExp
+var error = /[45]\d{2}/
+
+// A list of friendly name for the classes of HTTP status codes.
+//
+// statuses :: [String]
 var statuses = [ 'information'
                , 'success'
                , 'redirected'
                , 'client-error'
                , 'server-error' ]
 
+// A list of friendly name for a request's lifecycle's state.
+//
+// state-map :: [String]
 var state_map = [ 'unsent'
                 , 'opened'
                 , 'headers-received'
                 , 'loading'
                 , 'completed' ]
 
-function object_p(subject) {
-  return class_of(subject) == '[object Object]' }
-
-function status_type(status) {
-  var type = status.toString().charAt(0) - 1
-  return statuses[type] }
-
-function serialise_for_type(mime, data) {
-  return mime == 'application/json'?  JSON.stringify(data)
-  :      /* otherwise */              serialise(data) }
 
 
 //// -- Public interface ------------------------------------------------------
+
+///// Data active
+// A list of all active promises for HTTP requests.
+//
+// active :: [PromiseP]
 var active = []
 
+
+///// Object PromiseP <| Promise
+// A promise for an HTTP request.
+//
+// PromiseP :: Promise <| { "client"  -> XMLHttpRequest
+//                        , "uri"     -> String
+//                        , "options" -> { String -> String }}
 var PromiseP = Promise.derive({
+
+  ////// Function init
+  // Initialises an instance of a PromiseP.
+  //
+  // init! :: @this:Object* -> this
   init:
   function _init(client, uri, options) {
     Promise.init.call(this)
@@ -90,6 +149,16 @@ var PromiseP = Promise.derive({
 
     return this }
 
+
+  ////// Function fire
+  // Immediately invokes all functions registered for the given `event',
+  // even if the promise hasn't been resolved yet.
+  //
+  // Different from flushing, the invoked callbacks are not removed from
+  // the event's list of callbacks. So a callback may `fire' multiple
+  // times.
+  //
+  // fire :: @this:PromiseP, String, Any... -> this
 , fire:
   function _fire(event) {
     var args, callbacks, i, len
@@ -101,11 +170,24 @@ var PromiseP = Promise.derive({
 
     return this }
 
+
+  ////// Function forget
+  // Aborts a request and resolves the promise with a `forgotten`
+  // failure.
+  //
+  // forget :: @this:PromiseP* -> this
 , forget:
   function _forget() {
     this.client.abort()
     return this.flush('forgotten').fail('forgotten') }
 
+
+  ////// Function timeout
+  // Specifies the maximum amount of time (in seconds) the promise can
+  // take to be fulfilled. If it takes more time than time, the promise
+  // fails with a `timeout' error.
+  //
+  // timeout :: @this:PromiseP*, Number -> this
 , timeout: support_timeout_p?  function _timeout(delay) {
                                  this.timeout = delay * 1000
                                  return this }
@@ -119,11 +201,18 @@ var PromiseP = Promise.derive({
                                                         , delay * 1000 )
                                  return this }
 
+
+  ////// Function clear_timer
+  // Stops the timer for the promise. If one was previously set by
+  // invoking `timeout'.
+  //
+  // clear-timer :: @this:Promise* -> this
 , clear_timer: support_timeout_p?  function _clear_timer() {
                                      this.timeout = 0
                                      return this }
 
              : /* otherwise */     Promise.clear_timer
+
 
 // Generalised HTTP statuses
 , information  : register('status:information')
@@ -131,6 +220,7 @@ var PromiseP = Promise.derive({
 , redirected   : register('status:redirected')
 , client_error : register('status:client-error')
 , server_error : register('status:server-error')
+
 
 // Ready states
 , unsent           : register('state:unsent')
@@ -144,7 +234,22 @@ var PromiseP = Promise.derive({
 })
 
 
-
+///// Function request
+// Makes an HTTP request to the given URI, and returns a `PromiseP' that
+// such request will be fulfilled.
+//
+// Any actual work is carried over after the promise is returned from
+// this method. As such, the user can freely manipulate the promise
+// object synchronously before the connection with the endpoint is even
+// opened.
+//
+// Aside from the event queues flushed after the promise has been
+// fulfilled (or failed), the promise will also fire events from time to
+// time, or depending on certain occurrences — as soon as they
+// happen. Callbacks registered for those events may be invoked more
+// than once, and may be invoked before the promise is fulfilled.
+//
+// request :: String, { String -> String } -> PromiseP
 function request(uri, options) {
   var client, promise, method, serialise_body_p, mime
   options          = options         || {}
@@ -175,21 +280,31 @@ function request(uri, options) {
   return promise
 
 
+  // Sticks a serialised query and body object at the end of an URI.
+  // build-uri :: String, { String -> String }, { String -> String }? -> String
   function build_uri(uri, query, body) {
     uri = build_query_string(uri, query)
     return method == 'GET'?  build_query_string(uri, body)
     :      /* otherwise */   uri }
 
+  // Setups the headers for the HTTP request
+  // setup-headers :: { String -> String | [String] } -> Undefined
   function setup_headers(headers) {
     keys(headers).forEach(function(key) {
       client.setRequestHeader(key, headers[key]) })}
 
+  // Generates a handler for the given type of error
+  // make-error-handler :: String -> Event -> Undefined
   function make_error_handler(type) { return function(ev) {
     promise.flush(type).fail(type, ev) }}
 
+  // Invokes an error handler for the given type
+  // raise :: String -> Undefined
   function raise(type) {
     make_error_handler(type)() }
 
+  // Setups the event listeners for the HTTP request client
+  // setup-listeners :: () -> Undefined
   function setup_listeners() {
     client.onerror            = make_error_handler('errored')
     client.onabort            = make_error_handler('forgotten')
@@ -218,6 +333,10 @@ function request(uri, options) {
 }
 
 
+////// Function request_with_method
+// Generates a specialised request function for the given method.
+//
+// request-with-method :: String -> String, { String -> String } -> PromiseP
 function request_with_method(method) { return function(uri, options) {
   options = options || { }
   options.method = method.toUpperCase()
